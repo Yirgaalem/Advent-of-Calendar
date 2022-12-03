bcee375e61889026fc432d5177f25b54193d3646
Nahor Yirgaalem <82044598+Yirgaalem@users.noreply.github.com>
2022-12-03 13:31:59 -0500

    Update day2.py

--- Day2/day2.py
+++ Day2/day2.py
@@ -1,4 +1,5 @@
 '''
+PART 1
 Appreciative of your help yesterday, one Elf gives you an encrypted strategy guide (your puzzle input) that they say will be sure to help you win. "The first column is what your opponent is going to play: A for Rock, B for Paper, and C for Scissors. The second column--" Suddenly, the Elf is called away to help with someone's tent.
 
 The second column, you reason, must be what you should play in response: X for Rock, Y for Paper, and Z for Scissors. Winning every time would be suspicious, so the responses must have been carefully chosen.
@@ -19,9 +20,8 @@ The third round is a draw with both players choosing Scissors, giving you a scor
 In this example, if you were to follow the strategy guide, you would get a total score of 15 (8 + 1 + 6).
 
 What would your total score be if everything goes exactly according to your strategy guide?
-'''
+-----------------------------------------------------------------------------------------------------------
 
-'''
 Rock > Paper > Scissors > Rock
 COLUMN 1
 A - Rock
@@ -37,6 +37,19 @@ Selection - Rock is 1 point, Paper is 2 points, Scissors is 3 points
 Outcome - Win is 6 points, Draw is 3 points, Loss is 0 point
 
 The score of one round is the sum of the selection + outcome
+
+------------------------------------------------------------------------------------------------------------------------------------- 
+PART 2
+The Elf finishes helping with the tent and sneaks back over to you. "Anyway, the second column says how the round needs to end: X means you need to lose, Y means you need to end the round in a draw, and Z means you need to win. Good luck!"
+
+The total score is still calculated in the same way, but now you need to figure out what shape to choose so the round ends as indicated. The example above now goes like this:
+
+In the first round, your opponent will choose Rock (A), and you need the round to end in a draw (Y), so you also choose Rock. This gives you a score of 1 + 3 = 4.
+In the second round, your opponent will choose Paper (B), and you choose Rock so you lose (X) with a score of 1 + 0 = 1.
+In the third round, you will defeat your opponent's Scissors with Rock for a score of 1 + 6 = 7.
+Now that you're correctly decrypting the ultra top secret strategy guide, you would get a total score of 12.
+
+Following the Elf's instructions for the second column, what would your total score be if everything goes exactly according to your strategy guide?
 '''
 
 def part1(opponent, user):
